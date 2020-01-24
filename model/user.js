44ebb1640290eab605bb9d2cdd5ffb86ac391699
nilesh singh <kumarnile@gmail.com>
2020-01-24 23:29:19 +0530

    move private information into dev.js ;

--- model/user.js
+++ model/user.js
@@ -1,14 +1,36 @@
-const express = require('express');
-const app = express();
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://nilesh:singh1762@react-app-sbrn1.mongodb.net/test?retryWrites=true&w=majority', 
-  {useNewUrlParser: true}).then( () =>console.log('DBconen'))
-  .catch(err => console.error(err));
+const userSchema = mongoose.Schema({
+  name: {
+      type:String,
+      maxlength:50
+  },
+  email: {
+      type:String,
+      trim:true,
+      unique: 1 
+  },
+  password: {
+      type: String,
+      minglength: 5
+  },
+  lastname: {
+      type:String,
+      maxlength: 50
+  },
+  role : {
+      type:Number,
+      default: 0 
+  },
+  image: String,
+  token : {
+      type: String,
+  },
+  tokenExp :{
+      type: Number
+  }
+})
 
+ const User = mongoose.model('User', userSchema)
+ module.exports = { User }
 
-
-app.get('/', (req,res) => { 
-   res.send('Hello World')
-});
-
-app.listen(5000);
